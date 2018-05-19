0d9bc4b2bb0add5ee95dfc3cb3c7277d1a7f6a4b
Kevin Landa <kev.sam.landa@gmail.com>
2018-05-19 16:03:17 +0200

    baseUrl

--- colormind/dist/config.js
+++ colormind/dist/config.js
@@ -6,8 +6,9 @@ define(["require", "exports"], function (require, exports) {
             { test: /^\/?jquery/, result: "node_modules/jquery/dist/jquery" },
             { test: /^\/?bootstrap/, result: "node_modules/bootstrap/dist/js/bootstrap.bundle" },
             { test: /^\/?(node_modules\/*)/, result: "$1" },
-            { test: /^\/?(tools\/*)/, result: "dist/$1" },
-            { test: /^\/?artiste/, result: "node_modules/artistejs/dist/artiste" }
+            { test: /^\/?(tools\/*)/, result: "colormind/dist/$1" },
+            { test: /^\/?artiste/, result: "node_modules/artistejs/dist/artiste" },
+            { test: /^\/?(dist\/*)/, result: "colormind/dist/$1" }
         ]
     };
 });
