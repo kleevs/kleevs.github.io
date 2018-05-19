e237ce73bf82a2a53845cc6375c81ba5cdf4f024
Kevin Landa <kev.sam.landa@gmail.com>
2018-05-19 16:04:02 +0200

    baseUrl

--- colormind/dist/config.js
+++ colormind/dist/config.js
@@ -3,11 +3,11 @@ define(["require", "exports"], function (require, exports) {
     exports.__esModule = true;
     exports["default"] = {
         path: [
-            { test: /^\/?jquery/, result: "node_modules/jquery/dist/jquery" },
-            { test: /^\/?bootstrap/, result: "node_modules/bootstrap/dist/js/bootstrap.bundle" },
-            { test: /^\/?(node_modules\/*)/, result: "$1" },
+            { test: /^\/?jquery/, result: "colormind/node_modules/jquery/dist/jquery" },
+            { test: /^\/?bootstrap/, result: "colormind/node_modules/bootstrap/dist/js/bootstrap.bundle" },
+            { test: /^\/?(node_modules\/*)/, result: "colormind/$1" },
             { test: /^\/?(tools\/*)/, result: "colormind/dist/$1" },
-            { test: /^\/?artiste/, result: "node_modules/artistejs/dist/artiste" },
+            { test: /^\/?artiste/, result: "colormind/node_modules/artistejs/dist/artiste" },
             { test: /^\/?(dist\/*)/, result: "colormind/dist/$1" }
         ]
     };
